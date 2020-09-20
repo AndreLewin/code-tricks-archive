a3809de0c2cd55a3b6a981a4c5733c25a0bcae3e
AndreLewin <14772765+AndreLewin@users.noreply.github.com>
2020-09-20 19:02:00 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div style="background-color: grey;">
+    <select v-model="selectedComponent">
+      <option disabled value="">Choose the component to test</option>
+      <option>FlexboxWithGutters</option>
+    </select>
+  </div>
+
+  <template v-if="selectedComponent !== ''">
+    <component :is="selectedComponent"></component>
+  </template>
+</template>
+
+<script lang="ts">
+import FlexboxWithGutters from './components/css/FlexboxWithGutters.vue'
+
+import { defineComponent } from 'vue';
+export default defineComponent({
+  name: 'App',
+  components: {
+    FlexboxWithGutters
+  },
+  data() {
+    return {
+      selectedComponent: ''
+    }
+  }
+})
+</script>
---
+++ src/components/css/FlexboxWithGutters.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <span>Gutters are the space between the elements of the row. They are always the same width, but are not present outside of the flexbox. The trick is to apply a negative margin on the flexbox.</span>
+    <div class="flex-row-wrap with-gutters">
+      <div class="green-rectangle" />
+      <div class="green-rectangle" />
+      <div class="green-rectangle" />
+      <div class="green-rectangle" />
+      <div class="green-rectangle" />
+      <div class="green-rectangle" />
+      <div class="green-rectangle" />
+      <div class="green-rectangle" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+  name: 'FlexboxWithGutters'
+})
+</script>
+
+<style lang="scss" scoped>
+.flex-row-wrap {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+}
+
+$vertical-gutter-half-width: 20px;
+$horizontal-gutter-half-width: 10px;
+.with-gutters {
+  margin-left: -$vertical-gutter-half-width;
+  margin-right: -$vertical-gutter-half-width;
+  margin-top: -$horizontal-gutter-half-width;
+  margin-bottom: -$horizontal-gutter-half-width;
+
+  & > * {
+    margin-left: $vertical-gutter-half-width;
+    margin-right: $vertical-gutter-half-width;
+    margin-top: $horizontal-gutter-half-width;
+    margin-bottom: $horizontal-gutter-half-width;
+  }
+}
+
+.green-rectangle {
+  height: 100px;
+  width: 400px;
+  background: green;
+}
+</style>
